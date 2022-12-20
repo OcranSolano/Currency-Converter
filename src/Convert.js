24f520ebab079d184eac34b942a062ea31749c1e
OcranSolano <66854529+OcranSolano@users.noreply.github.com>
2022-12-19 22:23:09 -0600

    loading spinner for api call added

--- src/Convert.js
+++ src/Convert.js
@@ -1,4 +1,6 @@
-// import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
+
+import { useState } from "react";
 
 let myHeaders = new Headers();
 myHeaders.append("apikey", "1IBq1WjxEI5NMiLbXVxXsNPNnEKj8Mp7");
@@ -10,10 +12,13 @@ let requestOptions = {
 }
 
 export default function Convert(props) {
+
+    const [ loading, setLoading ] = useState(false);
     
     const amount = props.data[0];
     const from = props.data[1].slice(0,3);
     const to = props.data[2].slice(0,3);
+    const result = props.data[3];
 
     // useEffect(() => {
     //     if (amount.length === 0) {
@@ -34,11 +39,14 @@ export default function Convert(props) {
     // },[amount, from, to])
 
     function execute() {
+
         if (amount.length === 0) {
             alert('Please input AMOUNT to convert')
         } else if (props.data[1] === props.data[2] || amount === '0') {
             props.result(amount); //STRING
         } else {
+            setLoading(true)
+            
             fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
             /*.then(response => response.text())*/
             .then(response => response.json())
@@ -47,15 +55,19 @@ export default function Convert(props) {
             .then(console.log('DONE'))
             .catch(error => alert('error', error));
         }
-        console.log(from, to)
     }
+
+    useEffect(() => {
+        setLoading(false)
+    }, [result])
 
     return (
         <button 
             type='button' 
             className='btn btn-primary btn-lg convert'
-            onClick={execute}>
-                Convert
+            onClick={execute}
+            disabled={!loading ? false : true}>
+                {!loading ? 'Convert' : <span id='spinner' className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
         </button>
     )
 }
